fa37fd33df992cc1106f05b02fdc66f6f3b2b085
Manroop Dehal <manroopdehal17@gmail.com>
2026-03-02 01:10:54 -0500

    trying to figure out how to hide/unhide

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-      document.getElementById("stripedshirtButton").addEventListener("click", () => {
-        document.getElementById("stripedshirt").setAttribute("visible", "true");
-      });
+ stripedshirt = document.querySelector("#stripedshirt");
+ tanktop = document.querySelector("#tanktop");
+
+            function hideAllshirts() {
+                stripedshirt.setAttribute ("visible", false);
+                tank.setAttribute ("visible", false);
+            }
+
+            function showstripedshirt() {
+                hideAllshirts()
+                tanktop.setAttribute("visible", true);
+            }
