b1a3a4b6b8ef3f91ec6dbefb7ba6713de4386c93
Manroop Dehal <manroopdehal17@gmail.com>
2026-03-10 19:15:58 -0400

    Made stuff prettier, got rid of loading spin and just kept animation.

--- script.js
+++ script.js
@@ -78,12 +78,4 @@
                 hideAllshoes()
             }
 
-function loadbody() {
-  loader = setTimeout(showPage, 3000);
-}
-
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
-}
   
